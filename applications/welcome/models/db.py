a4eb899bf195f6dbd15fd6d262b20ad3b05fe2d4
Arvin <arvin@xc.xchg.com>
2011-06-21 14:30:51 -0600

    on xchg

--- applications/welcome/models/db.py
+++ applications/welcome/models/db.py
@@ -14,7 +14,7 @@ if request.env.web2py_runtime_gae:            # if running on Google App Engine
     # from google.appengine.api.memcache import Client
     # session.connect(request, response, db = MEMDB(Client()))
 else:                                         # else use a normal relational database
-    db = DAL('mysql://root:root@localhost/w2py')       # if not, use SQLite or other DB
+    db = DAL('mysql://arvin:aaa@69.164.212.73/arvin_web2py')       # if not, use SQLite or other DB
               #'sqlite://storage.sqlite'
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
